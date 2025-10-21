9a209d6dbc5e4f2509826c3cc7911881e9e2eabd
byteme <dkalhara41@gmail.com>
2025-10-21 23:05:13 +0530

    Created the city management function

--- Menu driven logistic management system/main.c
+++ Menu driven logistic management system/main.c
@@ -1,10 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int citymanagement(char *citynames,int x);
+
+
+
+int citymanagement(char *citynames,int x)
+{
+    int y=0;
+    int citycount;
+    printf("Type \"done\" when complete entering.");
+    do
+    {fgets(**citynames[y][50],50,stdin);
+    getchar();
+    y++;
+
+    }
+    while(strcmp(*citynames[y][50],done)!=0);
+    citycount=y-1;
+    return citycount;
+
+
+}
+
 int main()
 {
+    //int *citycount;
+    int x=30;
     int choice;
-    char citynames[50];
+    char *citynames[30][50];
 
     printf("Logistic Management System\n");
     printf("--------------------------\n\n\n");
@@ -12,14 +36,18 @@ int main()
 
     printf("1).City Management.\n");
     printf("2).Distance Management.\n");
-    printf("3).Vehicle Management.\n");
-    printf("4).Delivery Request Handling.\n");
-    printf("5).Cost,Time and Fuel Calculations.\n");
+    printf("3).Delivery Request Handling.\n");
+    printf("4).Cost,Time and Fuel Calculations.\n");
+    //printf("5)..\n");
     scanf("%d",&choice);
 
     switch(choice)
     {
     case 1:
+        void citymanagement(char *citynames,int x);
+
+    case 2:
+
 
 
     }
