9738b943f94c6fa5c134ed2cf7e15afb5c134989
byteme <dkalhara41@gmail.com>
2025-10-21 13:49:22 +0530

    Added main function

---
+++ Menu driven logistic management system/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int choice;
+    char citynames[50];
+
+    printf("Logistic Management System\n");
+    printf("--------------------------\n\n\n");
+    printf("-Menu-");
+
+    printf("1).City Management.\n");
+    printf("2).Distance Management.\n");
+    printf("3).Vehicle Management.\n");
+    printf("4).Delivery Request Handling.\n");
+    printf("5).Cost,Time and Fuel Calculations.\n");
+    scanf("%d",&choice);
+
+    switch(choice)
+    {
+    case 1:
+
+
+    }
+
+
+    return 0;
+}
