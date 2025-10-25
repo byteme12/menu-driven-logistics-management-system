7d8e36431923555a813e35d44a2c77997472274e
byteme <dkalhara41@gmail.com>
2025-10-25 14:11:06 +0530

    added city menu function

--- Menu driven logistic management system/main.c
+++ Menu driven logistic management system/main.c
@@ -6,7 +6,10 @@
 #define MAX_CITIES 30
 
 
-
+void addCity();
+void renameCity();
+void removeCity();
+void displayCities();
 void cityMenu();
 void distanceMenu();
 void reportMenu();
@@ -52,4 +55,40 @@ int main()
 
 
     return 0;
+}
+
+void cityMenu()
+{
+    int choice;
+    while (1)
+    {
+        printf("\n---- City Management ----\n");
+        printf("1. Add City\n);");
+        printf("2. Rename City\n");
+        printf("3. Remove City\n");
+        printf("4. Display Cities\n");
+        printf("0. Back\n");
+        printf("Choice: ");
+        scanf("%d", &choice);
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            addCity();
+            break;
+        case 2:
+            renameCity();
+            break;
+        case 3:
+            removeCity();
+            break;
+        case 4:
+            displayCities();
+            break;
+        case 0:
+            return;
+        default:
+            printf("Invalid!\n");
+        }
+    }
 }
