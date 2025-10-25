451922e43090286b8b26e8441e11e139580ccf77
byteme <dkalhara41@gmail.com>
2025-10-25 17:19:49 +0530

    Added functions in city management

--- Menu driven logistic management system/main.c
+++ Menu driven logistic management system/main.c
@@ -92,3 +92,65 @@ void cityMenu()
         }
     }
 }
+
+void addCity()
+{
+    if (cityCount >= MAX_CITIES)
+    {
+        printf("City limit reached.\n");
+        return;
+    }
+    printf("Enter city name: ");
+    scanf("%s", cities[cityCount]);
+    cityCount++;
+    printf("City added successfully.\n");
+}
+
+void renameCity()
+{
+    displayCities();
+    int index;
+    printf("Enter city index to rename: ");
+    scanf("%d", &index);
+    if (index < 0  ||index >= cityCount)
+    {
+        printf("Invalid index.\n");
+        return;
+    }
+    printf("Enter new name: ");
+    scanf("%s", cities[index-1]);
+    printf("City renamed.\n");
+}
+
+void removeCity()
+{
+    displayCities();
+    int index;
+    printf("Enter city index to remove: ");
+    scanf("%d", &index);
+    if (index < 0 || index >= cityCount)
+    {
+        printf("Invalid index.\n");
+        return;
+    }
+    for (int i = index; i < cityCount - 1; i++)
+    {
+        strcpy(cities[i], cities[i + 1]);
+        for (int j = 0; j < cityCount; j++)
+        {
+            distanceMatrix[i][j] = distanceMatrix[i+1][j];
+            distanceMatrix[j][i] = distanceMatrix[j][i+1];
+        }
+    }
+    cityCount--;
+    printf("City removed.\n");
+}
+
+void displayCities()
+{
+    printf("\nCities:\n");
+    for (int i = 0; i < cityCount; i++)
+    {
+        printf("%d. %s\n", i+1, cities[i]);
+    }
+}
